a735b9d1615142eaa62c737b141ae7b18d0f3794
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2026-02-12 11:21:13 +0100

    Thunderbird-extension: More %-escaping in URL.
    Closes: #3

--- thunderbird-extension/copy.js
+++ thunderbird-extension/copy.js
@@ -2,7 +2,7 @@
 // noinspection ExceptionCaughtLocallyJS
 
 // Add the button to the message display action toolbar
-browser.messageDisplayAction.onClicked.addListener(async (tab, info) => {
+browser.messageDisplayAction.onClicked.addListener(async (tab, _info) => {
   try {
     // Get the displayed message
     let message = await browser.messageDisplay.getDisplayedMessage(tab.id);
@@ -44,6 +44,18 @@ browser.messageDisplayAction.onClicked.addListener(async (tab, info) => {
   }
 });
 
+/** Like encodeURIComponent but encodes additional characters that make the URI more suitable for automatic recognition inside plain text */
+function myEncodeURIComponent(uriComponent) {
+  return encodeURIComponent(uriComponent)
+      .replace(/'/g, (char) => '%' + char.charCodeAt(0).toString(16).toUpperCase());
+}
+
+/** Optionally quotes the last character of the URI to make sure the URI is more suitable for automatic recognition inside plain text.
+ * E.g., doesn't end in a `.`. */
+function fixupURL(url) {
+  return url.replace(/[^a-zA-Z0-9/&#=]$/, (char) => '%' + char.charCodeAt(0).toString(16).toUpperCase());
+}
+
 // Construct the message link URL
 function constructUrl(message, options) {
   let url = options.baseUrl;
@@ -54,31 +66,33 @@ function constructUrl(message, options) {
     throw Error("Could not determine message id")
   
   // Start with message ID
-  url += "#" + encodeURIComponent(msgId);
+  url += "#" + myEncodeURIComponent(msgId);
   
   // Add optional fields
   if (options.includeSubject && message.subject) {
-    url += "&subject=" + encodeURIComponent(message.subject);
+    url += "&subject=" + myEncodeURIComponent(message.subject);
   }
   
   if (options.includeDate && message.date) {
     // Format date as ISO 8601
     let dateStr = new Date(message.date).toISOString();
-    url += "&date=" + encodeURIComponent(dateStr);
+    url += "&date=" + myEncodeURIComponent(dateStr);
   }
   
   if (options.includeFrom && message.author) {
-    url += "&from=" + encodeURIComponent(message.author);
+    url += "&from=" + myEncodeURIComponent(message.author);
   }
   
   if (options.includeTo && message.recipients && message.recipients.length > 0) {
     let toList = message.recipients.join(", ");
-    url += "&to=" + encodeURIComponent(toList);
+    url += "&to=" + myEncodeURIComponent(toList);
   }
   
   if (options.whoHasIt) {
-    url += "&whohasit=" + encodeURIComponent(options.whoHasIt);
+    url += "&whohasit=" + myEncodeURIComponent(options.whoHasIt);
   }
-  
+
+  url = fixupURL(url);
+
   return url;
 }
